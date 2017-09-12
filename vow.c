188426eebb44970cf3aa9d2e48ab6d0895d0d9b5
subidsha <31504698+subidsha@users.noreply.github.com>
2017-09-12 16:20:48 +0530

    Update and rename README.md to vow.c

---
+++ vow.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a;
+printf("enter the letter",a);
+scanf("%s",&a);
+{
+if(a||e||i||o||u==a)
+{
+printf("the letter is vowel");
+}
+else
+{
+printf("the letter is cosonant");
+}
+}}
