70c8b2917f1067fb86f5be5b5e3fb6a8374ec835
subidsha <31504698+subidsha@users.noreply.github.com>
2017-09-12 16:40:30 +0530

    Create cons.c

---
+++ cons.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char c;
+printf("the word is ",c);
+{
+if(a||e||i||o||u==c)
+{
+printf("the word is vowel");
+}
+else
+{
+printf("the word is consonant");
+}
+}
+}
